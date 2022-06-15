901b02295dc11065530ff8320e0e189eace41941
Menanya <emmmiemenz@gmail.com>
2022-06-15 03:00:01 +0100

    fix lintrs error

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import './style.css';
-import Scores from './modules/scores';
-
+import Scores from './modules/scores.js';
 
 const LeaderTable = document.getElementById('leaderBoard');
 
-for(let i = 0 ; i<Scores.length; i ++){
-    LeaderTable.innerHTML += `<li> ${Scores[i].Name}:  ${Scores[i].score} </li>`    
+for (let i = 0; i < Scores.length; i += 1) {
+  LeaderTable.innerHTML += `<li> ${Scores[i].Name}:  ${Scores[i].score} </li>`;
 }
--- src/modules/scores.js
+++ src/modules/scores.js
@@ -1,23 +1,22 @@
 const Scores = [{
-    Name: "Name",
-    score: 100,
-}, 
-{
-    Name: "Name",
-    score: 20,
+  Name: 'Name',
+  score: 100,
 },
 {
-    Name: "Name",
-    score: 50,
+  Name: 'Name',
+  score: 20,
 },
 {
-    Name: "Name",
-    score: 72,
+  Name: 'Name',
+  score: 50,
 },
 {
-    Name: "Name",
-    score: 200,
+  Name: 'Name',
+  score: 72,
+},
+{
+  Name: 'Name',
+  score: 200,
 }];
 
-
-export default Scores
+export default Scores;
