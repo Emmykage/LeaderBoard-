dff158404fdef972de6ec229afe5c01c5e56729b
Menanya <emmmiemenz@gmail.com>
2022-06-15 03:04:57 +0100

    fix linters

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './style.css';
 import Scores from './modules/scores.js';
 
 const LeaderTable = document.getElementById('leaderBoard');
-
 for (let i = 0; i < Scores.length; i += 1) {
   LeaderTable.innerHTML += `<li> ${Scores[i].Name}:  ${Scores[i].score} </li>`;
 }
