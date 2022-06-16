83d43aa8c429e7660f2a4246fc3c7c27cd338adb
Menanya <emmmiemenz@gmail.com>
2022-06-16 18:27:56 +0100

    finish API part 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,98 @@
 import './style.css';
-import Scores from './modules/scores.js';
+// import getID from './modules/ID';
+// import Scores from './modules/scores.js';
+
+const urlAPI = "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/";
+const ApiID = "rDBXUmKAIgUrA0R4BiuE";
+const completeUrl = `${urlAPI + ApiID}/scores/`;
+
+const refBtn = document.getElementById("refresh");
+const subBtn = document.getElementById("formId");
+
 
 const LeaderTable = document.getElementById('leaderBoard');
-for (let i = 0; i < Scores.length; i += 1) {
-  LeaderTable.innerHTML += `<li> ${Scores[i].Name}:  ${Scores[i].score} </li>`;
+
+// let Scores = [];
+
+
+// class ListObj {
+//   constructor(user, score){
+//     this.user = user;
+//     this.score = score;
+//   }
+// }
+
+
+// function addlist(user, score){
+//   const newList = new ListObj(user, score);
+//   // Scores.push(newList)
+//   // console.log(Scores);
+//   return newList
+
+// }
+function display (playList){
+  
+   
+  LeaderTable.innerHTML += `<li> ${playList.user}:  ${playList.score} </li>`;
+
 }
+
+async function sendData(){
+
+  const user = document.getElementById("name");
+const score = document.getElementById("score");
+  const response = await fetch ( "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/rDBXUmKAIgUrA0R4BiuE/scores/" , {
+    method: 'POST',
+    body: JSON.stringify({
+      user: user.value,
+      score: score.value
+    }),
+    headers: {
+      'content-type': 'application/json'
+    }
+  }
+  );
+  // console.log(play)
+  const sent = await response.json();
+  return sent
+
+}
+
+
+async function receiveData(){
+  LeaderTable.innerHTML = "";
+
+  const response = await fetch(completeUrl,{
+    method: 'GET',
+    header: {
+      'content-type': 'application/json; charset: UTF-8'
+    }
+  })
+
+  const gamelist = await response.json();
+  console.log(gamelist);
+  for(let i = 0 ; i < gamelist.result.length; i ++){
+     display(gamelist.result[i])
+  }
+  // display(gamelist)
+  return gamelist
+
+
+}
+
+
+subBtn.addEventListener("submit", (e)=>{
+
+  e.preventDefault();
+  
+  sendData();
+  // console.log(newEntry);
+
+});
+receiveData()
+
+refBtn.addEventListener("click", ()=>{
+  receiveData()
+  
+})
+
--- src/modules/scores.js
+++ src/modules/scores.js
@@ -1,22 +1,5 @@
-const Scores = [{
-  Name: 'Name',
-  score: 100,
-},
-{
-  Name: 'Name',
-  score: 20,
-},
-{
-  Name: 'Name',
-  score: 50,
-},
-{
-  Name: 'Name',
-  score: 72,
-},
-{
-  Name: 'Name',
-  score: 200,
-}];
+const Scores = [
+  
+];
 
 export default Scores;
